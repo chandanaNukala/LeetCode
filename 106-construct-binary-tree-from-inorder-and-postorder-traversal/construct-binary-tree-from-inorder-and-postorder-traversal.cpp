b71281dabb7b50afb72d4b027e2e62a5410247ab
Chandana_nukala <66661365+chandanaNukala@users.noreply.github.com>
2025-05-26 23:40:08 -0400

    Time: 3 ms (69.80%) | Memory: 27.5 MB (58.65%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,21 +11,43 @@
  */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        if(inorder.size()==0) return NULL;
-        
+    unordered_map<int,int>in_indices;
+    int pre_ptr=0;
+
+    TreeNode* helper(vector<int>&postorder, int l,int r){
+        if(l>r) return NULL;
         TreeNode* root= new TreeNode(postorder[postorder.size()-1]);
         postorder.pop_back();
 
-        auto it=find(inorder.begin(),inorder.end(),root->val);
-        auto root_inorder= it-inorder.begin();
+        int mid_inorder=in_indices[root->val];
+       
+        root->right=helper(postorder,mid_inorder+1,r);
+        root->left=helper(postorder,l,mid_inorder-1);
+        
 
-        vector<int> right_inorder(inorder.begin()+root_inorder+1,inorder.end());
-        vector<int>left_inorder(inorder.begin(),inorder.begin()+root_inorder);
-        root->right=buildTree(right_inorder,postorder);
-        root->left=buildTree(left_inorder,postorder);
+        return root;
 
-         return root;
+    }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+
+        // if(inorder.size()==0) return NULL;
+        
+        // TreeNode* root= new TreeNode(postorder[postorder.size()-1]);
+        // postorder.pop_back();
+
+        // auto it=find(inorder.begin(),inorder.end(),root->val);
+        // auto root_inorder= it-inorder.begin();
+
+        // vector<int> right_inorder(inorder.begin()+root_inorder+1,inorder.end());
+        // vector<int>left_inorder(inorder.begin(),inorder.begin()+root_inorder);
+        // root->right=buildTree(right_inorder,postorder);
+        // root->left=buildTree(left_inorder,postorder);
+
+        //  return root;
+        for(int i=0;i<inorder.size();i++){
+            in_indices[inorder[i]]=i;
+        }
+        return helper(postorder,0,inorder.size()-1);
 
     }
 };
