84a4eddf16c4eb4c45037ccd908f60b077839f53
Chandana_nukala <66661365+chandanaNukala@users.noreply.github.com>
2025-05-26 23:40:20 -0400

    Time: 4 ms (51.05%) | Memory: 27.4 MB (58.65%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -30,20 +30,6 @@ public:
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
 
-        // if(inorder.size()==0) return NULL;
-        
-        // TreeNode* root= new TreeNode(postorder[postorder.size()-1]);
-        // postorder.pop_back();
-
-        // auto it=find(inorder.begin(),inorder.end(),root->val);
-        // auto root_inorder= it-inorder.begin();
-
-        // vector<int> right_inorder(inorder.begin()+root_inorder+1,inorder.end());
-        // vector<int>left_inorder(inorder.begin(),inorder.begin()+root_inorder);
-        // root->right=buildTree(right_inorder,postorder);
-        // root->left=buildTree(left_inorder,postorder);
-
-        //  return root;
         for(int i=0;i<inorder.size();i++){
             in_indices[inorder[i]]=i;
         }
