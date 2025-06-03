16ef942ab81ca38192275e17db9ec34c658571eb
Chandana_nukala <66661365+chandanaNukala@users.noreply.github.com>
2025-06-03 16:49:41 -0400

    Time: 0 ms (100.00%) | Memory: 7.7 MB (89.98%) - LeetSync

--- 1086-divisor-game/divisor-game.cpp
+++ 1086-divisor-game/divisor-game.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        vector<int>dp(n+1,0);
-        dp[0]=0;
-        dp[1]=0;
-        for(int i=2;i<n+1;i++){
-            for(int x=1;x<i;x++){
-                if(i%x==0 && !dp[i-x]){
-                    dp[i]=1;
-                    break;
-                }
-            }
-        }
-        return dp[n];
+        // vector<int>dp(n+1,0);
+        // dp[0]=0;
+        // dp[1]=0;
+        // for(int i=2;i<n+1;i++){
+        //     for(int x=1;x<i;x++){
+        //         if(i%x==0 && !dp[i-x]){
+        //             dp[i]=1;
+        //             break;
+        //         }
+        //     }
+        // }
+        // return dp[n];
+
+        if(n%2==0) return true;
+        return false;
     }
 };
