9ac686461e0ac374004fcd3f71fb0f1402cfad0c
Chandana_nukala <66661365+chandanaNukala@users.noreply.github.com>
2025-06-28 12:21:38 -0400

    Time: 30 ms (38.43%) | Memory: 23 MB (36.17%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,38 @@
+class Solution {
+  vector<pair<int,int>>directions={{0,1},{0,-1},{1,0},{-1,0}};
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int islands=0;
+        for(int r=0;r<grid.size();r++){
+            for(int c=0;c<grid[0].size();c++){
+                if(grid[r][c]=='1'){
+                    bfs(grid,r,c);
+                    islands++;
+                }
+            }
+        }
+        return islands;
+    }
+
+    void bfs(vector<vector<char>>& grid,int r,int c){
+        queue<pair<int,int>>q;
+        grid[r][c]='0';
+        q.push({r,c});
+
+        while(!q.empty()){
+            auto node=q.front();q.pop();
+            int row=node.first,col=node.second;
+
+            for(const auto&d:directions ){
+                int row_temp=row+d.first;
+                int col_temp=col+d.second;
+
+                if(row_temp>=0 && row_temp<grid.size() && col_temp>=0 && col_temp<grid[0].size() && grid[row_temp][col_temp]=='1'){
+                    q.push({row_temp,col_temp});
+                    grid[row_temp][col_temp]='0';
+
+                }
+            }
+        }
+    }
+};
