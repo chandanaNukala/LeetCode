ce195acd0a2818a6c5a7b9ec23af5d963fc2bf3e
Chandana_nukala <66661365+chandanaNukala@users.noreply.github.com>
2025-07-02 15:19:22 -0400

    Time: 3 ms (73.99%) | Memory: 13.3 MB (35.96%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -10,17 +10,29 @@ public:
             {'D', 500},
             {'M', 1000}
         };
-        int res=0;
-        for(int i=0;i<s.size();){
-            if(map[s[i]]<map[s[i+1]]){
-                res+= map[s[i+1]]-map[s[i]];
-                i=i+2;  
+        // int res=0;
+        // for(int i=0;i<s.size();){
+        //     if(map[s[i]]<map[s[i+1]]){
+        //         res+= map[s[i+1]]-map[s[i]];
+        //         i=i+2;  
+        //     }
+        //     else{
+        //         res+=map[s[i]];
+        //         i++;
+        //     }
+            
+        // }
+        // return res;
+        int lastval=map[s[s.length()-1]];
+        int res=lastval;
+        for(int i=s.length()-2;i>=0;i--){
+            if(map[s[i]]<lastval){
+                res-=map[s[i]];
             }
             else{
                 res+=map[s[i]];
-                i++;
             }
-            
+            lastval=map[s[i]];
         }
         return res;
     }
