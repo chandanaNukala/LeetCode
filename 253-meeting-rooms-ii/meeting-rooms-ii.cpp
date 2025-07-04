095a0e113e19dac7ea31921a0626bc19bcb4dbb9
Chandana_nukala <66661365+chandanaNukala@users.noreply.github.com>
2025-07-04 13:37:35 -0400

    Time: 3 ms (57.64%) | Memory: 16 MB (94.50%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/meeting-rooms-ii.cpp
@@ -1,32 +1,42 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        vector<int>start;
-        vector<int>end;
-        for(int i=0;i<intervals.size();i++){
-            start.push_back(intervals[i][0]);
-            end.push_back(intervals[i][1]);
-        }
-        sort(start.begin(),start.end());
-        sort(end.begin(),end.end());
+        // vector<int>start;
+        // vector<int>end;
+        // for(int i=0;i<intervals.size();i++){
+        //     start.push_back(intervals[i][0]);
+        //     end.push_back(intervals[i][1]);
+        // }
+        // sort(start.begin(),start.end());
+        // sort(end.begin(),end.end());
 
-        int s=0;
-        int e=0;
-        int count=0;
-        int ans=0;
-        while(s!=intervals.size()){
-            if(start[s]<end[e]){
-                s++;
-                count++;
+        // int s=0;
+        // int e=0;
+        // int count=0;
+        // int ans=0;
+        // while(s!=intervals.size()){
+        //     if(start[s]<end[e]){
+        //         s++;
+        //         count++;
                 
+        //     }
+        //     else{
+        //         e++;
+        //         count--;
+        //     }
+        //     ans=max(ans,count);
+        // }
+        // return ans;
+        priority_queue<int,vector<int>,greater<>>pq;
+        sort(intervals.begin(),intervals.end());
+        int ans=0;
+        for(int i=0;i<intervals.size();i++){
+            if(!pq.empty() && pq.top()<=intervals[i][0]){ 
+                pq.pop();
             }
-            else{
-                e++;
-                count--;
-            }
-            ans=max(ans,count);
+            pq.push(intervals[i][1]);
+            ans=max(ans,(int)pq.size());
         }
         return ans;
-
     }
 };
