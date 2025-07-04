e86ece0580ea3060019d4db7377a59ffbca2fd64
Chandana_nukala <66661365+chandanaNukala@users.noreply.github.com>
2025-07-04 11:23:22 -0400

    Time: 229 ms (75.53%) | Memory: 111.9 MB (66.38%) - LeetSync

--- 3905-partition-string/partition-string.cpp
+++ 3905-partition-string/partition-string.cpp
@@ -2,15 +2,26 @@
 class Solution {
 public:
     vector<string> partitionString(string s) {
+        // vector<string>ans;
+        // unordered_map<string,int>map;
+        // string temp="";
+        // for(int i=0;i<s.length();i++){
+        //     temp+=s[i];
+        //     if(map[temp]==0){
+        //         ans.push_back(temp);
+        //         map[temp]++;
+        //         temp="";
+        //     }
+        // }
+        // return ans;
         vector<string>ans;
-        unordered_map<string,int>map;
-
+        unordered_set<string>set;
         string temp="";
         for(int i=0;i<s.length();i++){
             temp+=s[i];
-            if(map[temp]==0){
+            if(set.find(temp)==set.end()){
                 ans.push_back(temp);
-                map[temp]++;
+                set.insert(temp);
                 temp="";
             }
         }
