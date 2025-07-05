6a96f4be4099648e355911c6056b72c399395b71
Chandana_nukala <66661365+chandanaNukala@users.noreply.github.com>
2025-07-04 21:35:12 -0400

    Time: 0 ms (100.00%) | Memory: 21.6 MB (93.79%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -2,8 +2,24 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
 
-        intervals.push_back(newInterval);
-        sort(intervals.begin(),intervals.end());
+        int l=0;
+        int r=intervals.size()-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(intervals[mid][0]>newInterval[0]){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        // if(intervals.size()%2==0){
+        //     intervals.insert(intervals.begin() + l, newInterval);
+        // }
+        // else{
+        //     intervals.insert(intervals.begin() + l+1, newInterval);
+        // }
+        intervals.insert(intervals.begin() + l, newInterval);
         
         vector<vector<int>>merged;
         for(int i=0;i<intervals.size();i++){
