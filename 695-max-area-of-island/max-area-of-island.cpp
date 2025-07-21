3e49be87ff85e2082e7ee689f1f789743dd5247c
Chandana_nukala <66661365+chandanaNukala@users.noreply.github.com>
2025-07-21 12:53:35 -0400

    Time: 0 ms (100.00%) | Memory: 31.1 MB (24.11%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -6,25 +6,31 @@ public:
         for(int r=0;r<grid.size();r++){
             for(int c=0;c<grid[0].size();c++){
                 if(grid[r][c]==1){
-                    area=max(area,dfs(grid,r,c));
+                    area=max(area,bfs(grid,r,c));
                 }
             }
         }
         return area;
     }
-    int dfs(vector<vector<int>>& grid, int r, int c) {
-      
-        if(r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size() || grid[r][c] != 1) {
-            return 0;
-        }
-
-        grid[r][c] = 0; 
+    int bfs(vector<vector<int>>& grid,int r,int c){
+        queue<pair<int,int>>q;
+        grid[r][c]=0;
+        q.push({r,c});
+        
         int res=1;
-
-        for(const auto& d : directions) {
-            int new_r = r + d.first;
-            int new_c = c + d.second;
-            res+=dfs(grid, new_r, new_c);
+        while(!q.empty()){
+            auto node=q.front();
+            q.pop();
+            int row = node.first, col = node.second;
+            for(auto d:directions){
+                int tmp_r=row+d.first;
+                int tmp_c=col+d.second;
+                if(tmp_r<grid.size() && tmp_r>=0 && tmp_c<grid[0].size() && tmp_c>=0 && grid[tmp_r][tmp_c]==1){
+                    grid[tmp_r][tmp_c]=0;
+                    q.push({tmp_r,tmp_c});
+                    res++;
+                }
+            }
         }
         return res;
     }
