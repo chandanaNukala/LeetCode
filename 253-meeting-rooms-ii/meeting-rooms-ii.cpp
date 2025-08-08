d226d2d31b0f36fca0aeab8db8c64cb567aa5efa
Chandana_nukala <66661365+chandanaNukala@users.noreply.github.com>
2025-08-07 22:59:15 -0400

    Time: 0 ms (100.00%) | Memory: 16.1 MB (83.36%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.cpp
+++ 253-meeting-rooms-ii/meeting-rooms-ii.cpp
@@ -1,42 +1,16 @@
 class Solution {
 public:
     int minMeetingRooms(vector<vector<int>>& intervals) {
-        // vector<int>start;
-        // vector<int>end;
-        // for(int i=0;i<intervals.size();i++){
-        //     start.push_back(intervals[i][0]);
-        //     end.push_back(intervals[i][1]);
-        // }
-        // sort(start.begin(),start.end());
-        // sort(end.begin(),end.end());
-
-        // int s=0;
-        // int e=0;
-        // int count=0;
-        // int ans=0;
-        // while(s!=intervals.size()){
-        //     if(start[s]<end[e]){
-        //         s++;
-        //         count++;
-                
-        //     }
-        //     else{
-        //         e++;
-        //         count--;
-        //     }
-        //     ans=max(ans,count);
-        // }
-        // return ans;
-        priority_queue<int,vector<int>,greater<>>pq;
-        sort(intervals.begin(),intervals.end());
-        int ans=0;
-        for(int i=0;i<intervals.size();i++){
-            if(!pq.empty() && pq.top()<=intervals[i][0]){ 
-                pq.pop();
-            }
-            pq.push(intervals[i][1]);
-            ans=max(ans,(int)pq.size());
+       priority_queue<int,vector<int>,greater<int>>pq;
+       sort(intervals.begin(),intervals.end());
+       int cnt=0;
+       for(int i=0;i<intervals.size();i++){
+        if(!pq.empty() && pq.top()<=intervals[i][0]){
+            pq.pop();
         }
-        return ans;
+        pq.push(intervals[i][1]);
+        cnt=max(cnt,(int)pq.size());
+       }
+       return cnt;
     }
 };
