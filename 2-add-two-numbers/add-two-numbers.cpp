0bb4a35b8d7c21fe0597347d088ef60a2fdf2968
Chandana_nukala <66661365+chandanaNukala@users.noreply.github.com>
2025-08-13 17:03:13 -0400

    Time: 3 ms (34.06%) | Memory: 77.1 MB (43.91%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,19 +11,19 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* list=new ListNode(0);
-        ListNode* cur=list;
+        ListNode* LL=new ListNode(0);
+        ListNode* cur=LL;
         int carry=0;
-        while(l1!=NULL || l2!=NULL ||carry!=0){
+        while(l1!=NULL || l2!=NULL || carry!=0){
             int x=l1? l1->val:0;
-            int y=l2? l2->val:0; 
+            int y=l2? l2->val:0;
             int sum=x+y+carry;
             carry=sum/10;
             cur->next=new ListNode(sum%10);
             cur=cur->next;
-            l1=l1? l1->next : NULL;
-            l2=l2? l2->next: NULL;
+            l1=l1?l1->next:NULL;
+            l2=l2?l2->next:NULL;
         }
-        return list->next;
+        return LL->next;
     }
 };
