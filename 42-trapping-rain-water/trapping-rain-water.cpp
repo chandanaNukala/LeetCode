1ecf2117b893a9cfb2fea12fe27ffc9e22604ce3
Chandana_nukala <66661365+chandanaNukala@users.noreply.github.com>
2025-08-13 17:45:33 -0400

    Time: 6 ms (5.91%) | Memory: 27.4 MB (19.96%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        // int max_l=height[0];
-        // int max_r=height[height.size()-1];
-        // int l=0;
-        // int r=height.size()-1;
         // int ans=0;
-        // while(l<r){
-        //     if(max_l<=max_r){
-        //         ans+=max_l-height[l];
-        //         l++;
-        //         max_l=max(max_l,height[l]);
+        // for(int i=0;i<height.size();i++){
+        //     int left_max=0,right_max=0;
+        //     for(int j=i;j>=0;j--){
+        //         left_max=max(left_max,height[j]);
         //     }
-        //     else{
-        //         ans+=max_r-height[r];
-        //         r--;
-        //         max_r=max(max_r,height[r]);
+        //     for(int j=i;j<height.size();j++){
+        //         right_max=max(right_max,height[j]);
         //     }
+        //     ans+=min(left_max,right_max)-height[i];
         // }
         // return ans;
 
         int ans=0;
-        int ptr=0;
-        stack<int>st;
-        while(ptr<height.size()){
-            while(!st.empty() && height[ptr]>height[st.top()]){
-                int top=st.top();
-                st.pop();
-                if(st.empty()) break ; //for edge cases
-                int dist=ptr-st.top()-1;  // distance between left and right of popped
-                ans+= dist*(min(height[ptr],height[st.top()])-height[top]);
-             }
-             st.push(ptr++);
+        int n=height.size();
+        vector<int> left(n),right(n);
+        left[0]=height[0];
+        for(int i=1;i<n;i++){
+            left[i]=max(left[i-1],height[i]);
+        }
+        right[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            right[i]=max(right[i+1],height[i]);
+        }
+        for(int i=1;i<n-1;i++){
+            ans+=min(left[i],right[i])-height[i];
         }
         return ans;
     }
